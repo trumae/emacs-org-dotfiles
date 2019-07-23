cdfdf9eddec941b238e1d5148438d45f7386e49a
trumae <trumae@gmail.com>
2019-07-23 19:44:05 -0300

    title

--- sensitive/custom.el
+++ sensitive/custom.el
@@ -5,9 +5,10 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(org-trello-current-prefix-keybinding "C-c o" nil (org-trello))
  '(package-selected-packages
    (quote
-    (beacon sublimity google-translate-default-ui google-translate ack symon magit nov neotree all-the-icons projectile racer rust-mode rainbow-delimiters geiser godoctor go-mode forth-mode company-c-headers company cmake-ide rtags nasm-mode flycheck highlight-numbers telephone-line kaolin-themes use-package slime))))
+    (org-trello beacon sublimity google-translate-default-ui google-translate ack symon magit nov neotree all-the-icons projectile racer rust-mode rainbow-delimiters geiser godoctor go-mode forth-mode company-c-headers company cmake-ide rtags nasm-mode flycheck highlight-numbers telephone-line kaolin-themes use-package slime))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
